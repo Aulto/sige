a9006802558cccf12acd10fac16b9dcde90f70fa
Giovanni Paolo <giovanni1995@gmail.com>
2013-04-20 06:04:19 -0300

    remover testes, documentacao ISige

--- src/sige/sistema/ISige.java
+++ src/sige/sistema/ISige.java
@@ -7,18 +7,54 @@ import sige.repositorio.RepositorioException;
 
 public interface ISige {
 
+	/**
+	 * Buscar pessoas pelo cpf
+	 * @param cpf
+	 * 		cpf para buscar
+	 * @return
+	 * 		retorna arraylist com todos os resultados da busca
+	 * @throws ProblemaInterno
+	 * 		lançado quando ocorre algum problema no sistema
+	 */
 	ArrayList<Pessoa> buscarCpf(String cpf) throws ProblemaInterno;
 
+	/**
+	 * Buscar pessoas pelo id
+	 * @param id
+	 * 		id a ser buscado
+	 * @return
+	 * 		retorna arraylist com os resultados da busca
+	 * @throws ProblemaInterno
+	 * 		lançado quando ocorre algum problema no sistema
+	 */
 	ArrayList<Pessoa> buscarId(int id) throws ProblemaInterno;
 
+	/**
+	 * Verificar se há um administrador no sistema
+	 * @return
+	 * 		retorna true caso haja um, false caso contrário
+	 * @throws RepositorioException
+	 * 		lançado quando há problemas na conexão com o repositório
+	 */
 	boolean autenticarSistema() throws RepositorioException;
 
+	/**
+	 * Adicionar Aluno no repositório
+	 * @throws ProblemaInterno
+	 * 		lançado quando ocorre algum problema no sistema
+	 */
 	void adicionarAluno(String nome, String cpf, String rg, String senha,
 			String sexo, String estadoCivil, String dataNascimento,
 			String email, String telefone, String celular, String rua,
 			String bairro, String cidade, String uf, String complemento,
 			String cep, String numero, String pais) throws ProblemaInterno;
 
+	/**
+	 * Adicionar ProfessorAdministrador no repositório
+	 * @throws AutenticacaoException
+	 * @throws ProblemaInterno
+	 * 		lançado quando há problemas na conexão com o repositório
+	 */
 	void adicionarProfessorAdm(Class<?> tipo, String nome, String cpf,
 			String rg, String senha, String sexo, String estadoCivil,
 			String dataNascimento, String email, String telefone,
@@ -26,11 +62,36 @@ public interface ISige {
 			String uf, String complemento, String cep, String numero,
 			String pais) throws AutenticacaoException, ProblemaInterno;
 
+	/**
+	 * Buscar os administradores registrados no sistema
+	 * @return
+	 * 		retorna uma arraylist de pessoas com todos os administradores cadastrados
+	 * @throws AutenticacaoException
+	 * @throws ProblemaInterno
+	 * 		lançado quando há problemas na conexão com o repositório
+	 */
 	ArrayList<Pessoa> buscaAdm() throws AutenticacaoException, ProblemaInterno;
 
+	/**
+	 * Buscar administrador por nome
+	 * @param nome
+	 * 		nome a ser buscado
+	 * @return
+	 * 		retorna os resultados da busca em forma de arraylist
+	 * @throws AutenticacaoException
+	 * @throws ProblemaInterno
+	 * 		lançado quando ocorre algum problema no sistema
+	 */
 	ArrayList<Pessoa> buscaAdmNome(String nome) throws AutenticacaoException,
 			ProblemaInterno;
 
+	/**
+	 * Buscar administrador a partir do cpf
+	 * @param cpf
+	 * @return
+	 * @throws AutenticacaoException
+	 * @throws ProblemaInterno
+	 */
 	ArrayList<Pessoa> buscaAdmCpf(String cpf) throws AutenticacaoException,
 			ProblemaInterno;
 
